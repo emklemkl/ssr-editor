208cbb8ebb3ecd3a7e6187a54a34289a99f40b35
emklemkl <emil94_karlsson@outlook.com>
2024-10-14 16:35:26 +0200

    fixed a bug where the test env tried to start multiple instances of the server on the same port.

--- app.js
+++ app.js
@@ -37,52 +37,52 @@ if (process.env.NODE_ENV !== 'test') {
 
         app.use("/document", document);
         app.use("/sandbox", sandbox);
-        let myRoom;
-        let gotUpdate = false;
+            let myRoom;
+            let gotUpdate = false;
 
-        io.sockets.on('connect', async function (socket) {
-            console.log("Socket Id:", socket.id); // Nått lång och slumpat
-            const collection = await getCollection(db, "crowd");
+            io.sockets.on('connect', async function (socket) {
+                console.log("Socket Id:", socket.id); // Nått lång och slumpat
+                const collection = await getCollection(db, "crowd");
 
-            socket.on('create', async function (room) {
-                myRoom = room;
-                socket.join(room);
-                const res = await collection.findOne({ _id: new ObjectId(room) });
+                socket.on('create', async function (room) {
+                    myRoom = room;
+                    socket.join(room);
+                    const res = await collection.findOne({ _id: new ObjectId(room) });
 
-                io.to(room).emit("doc-update", res);
+                    io.to(room).emit("doc-update", res);
+                });
+
+                socket.on("doc-update", async (res) => {
+                    const parsedRes = JSON.parse(res);
+                    const { _id, ...rest } = parsedRes;
+
+                    try {
+                        await collection.updateOne({ _id: ObjectId.createFromHexString(_id) }
+                            , { $set: rest });
+                        gotUpdate = true;
+                    } catch (e) {
+                        console.error("Error updating document:", e);
+                    }
+                });
+
+                setInterval(async () => {
+                    if (gotUpdate) {
+                        const res = await collection.findOne({ _id: new ObjectId(myRoom) });
+
+                        io.to(myRoom).emit("doc-update", res);
+                    } gotUpdate = false;
+                }, 2000);
             });
 
-            socket.on("doc-update", async (res) => {
-                const parsedRes = JSON.parse(res);
-                const { _id, ...rest } = parsedRes;
-
-                try {
-                    await collection.updateOne({ _id: ObjectId.createFromHexString(_id) }
-                        , { $set: rest });
-                    gotUpdate = true;
-                } catch (e) {
-                    console.error("Error updating document:", e);
-                }
+            httpServer.listen(port, () => {
+                console.log(`\nPort ${port} set (if local: http://localhost:5000/)\n`);
+                console.log(`Server is running in ${process.env.NODE_ENV.toUpperCase()} mode \n`);
             });
-
-            setInterval(async () => {
-                if (gotUpdate) {
-                    const res = await collection.findOne({ _id: new ObjectId(myRoom) });
-
-                    io.to(myRoom).emit("doc-update", res);
-                } gotUpdate = false;
-            }, 2000);
-        });
-
-        httpServer.listen(port, () => {
-            console.log(`\nPort ${port} set (if local: http://localhost:5000/)\n`);
-            console.log(`Server is running in ${process.env.NODE_ENV.toUpperCase()} mode \n`);
-        });
     } catch (error) {
         console.error("Failed to connect to the database:", error);
         process.exit(1);
     }
 })();
 
-export default app;
+export default httpServer;
 
