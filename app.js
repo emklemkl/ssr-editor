da14c2793c827297a0aa4c91e1f29aa8a9715cd9
emklemkl <emil94_karlsson@outlook.com>
2024-10-14 16:40:50 +0200

    linting errors fixed

--- app.js
+++ app.js
@@ -37,47 +37,47 @@ if (process.env.NODE_ENV !== 'test') {
 
         app.use("/document", document);
         app.use("/sandbox", sandbox);
-            let myRoom;
-            let gotUpdate = false;
+        let myRoom;
+        let gotUpdate = false;
 
-            io.sockets.on('connect', async function (socket) {
-                console.log("Socket Id:", socket.id); // Nått lång och slumpat
-                const collection = await getCollection(db, "crowd");
+        io.sockets.on('connect', async function (socket) {
+            console.log("Socket Id:", socket.id); // Nått lång och slumpat
+            const collection = await getCollection(db, "crowd");
 
-                socket.on('create', async function (room) {
-                    myRoom = room;
-                    socket.join(room);
-                    const res = await collection.findOne({ _id: new ObjectId(room) });
+            socket.on('create', async function (room) {
+                myRoom = room;
+                socket.join(room);
+                const res = await collection.findOne({ _id: new ObjectId(room) });
 
-                    io.to(room).emit("doc-update", res);
-                });
-
-                socket.on("doc-update", async (res) => {
-                    const parsedRes = JSON.parse(res);
-                    const { _id, ...rest } = parsedRes;
-
-                    try {
-                        await collection.updateOne({ _id: ObjectId.createFromHexString(_id) }
-                            , { $set: rest });
-                        gotUpdate = true;
-                    } catch (e) {
-                        console.error("Error updating document:", e);
-                    }
-                });
-
-                setInterval(async () => {
-                    if (gotUpdate) {
-                        const res = await collection.findOne({ _id: new ObjectId(myRoom) });
-
-                        io.to(myRoom).emit("doc-update", res);
-                    } gotUpdate = false;
-                }, 2000);
+                io.to(room).emit("doc-update", res);
             });
 
-            httpServer.listen(port, () => {
-                console.log(`\nPort ${port} set (if local: http://localhost:5000/)\n`);
-                console.log(`Server is running in ${process.env.NODE_ENV.toUpperCase()} mode \n`);
+            socket.on("doc-update", async (res) => {
+                const parsedRes = JSON.parse(res);
+                const { _id, ...rest } = parsedRes;
+
+                try {
+                    await collection.updateOne({ _id: ObjectId.createFromHexString(_id) }
+                        , { $set: rest });
+                    gotUpdate = true;
+                } catch (e) {
+                    console.error("Error updating document:", e);
+                }
             });
+
+            setInterval(async () => {
+                if (gotUpdate) {
+                    const res = await collection.findOne({ _id: new ObjectId(myRoom) });
+
+                    io.to(myRoom).emit("doc-update", res);
+                } gotUpdate = false;
+            }, 2000);
+        });
+
+        httpServer.listen(port, () => {
+            console.log(`\nPort ${port} set (if local: http://localhost:5000/)\n`);
+            console.log(`Server is running in ${process.env.NODE_ENV.toUpperCase()} mode \n`);
+        });
     } catch (error) {
         console.error("Failed to connect to the database:", error);
         process.exit(1);
